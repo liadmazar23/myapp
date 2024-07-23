6710877915fd65e4475c1b268ec104ac8a63d1cb
liadmazar23 <139764769+liadmazar23@users.noreply.github.com>
2024-07-23 15:32:55 +0300

    setting new folders foec2 and ecr

---
+++ infra/ec2-jenkins/ec2-jenkins.tf
@@ -0,0 +1,67 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_security_group" "jenkins_sg" {
+  name        = "jenkins_sg"
+  description = "Security group for Jenkins instance"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["46.117.189.39/32"] 
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["46.117.189.39/32"] 
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "jenkins" {
+  ami                    = "ami-0427090fd1714168b" # Amazon Linux 2 AMI
+  instance_type          = "t2.micro"
+  key_name               = "us-east-1-key" 
+  security_groups        = [aws_security_group.jenkins_sg.name]
+  associate_public_ip_address = true
+
+  user_data = <<-EOF
+              #!/bin/bash
+
+              # Update package index
+              sudo yum update -y
+
+              # Install Docker
+              sudo yum install docker -y
+              
+              # Start Docker service
+              sudo service docker start
+
+              # Wait for Docker service to start
+              sleep 5
+
+              # Pull and run the Jenkins Docker container
+              sudo docker run -d -p 8080:8080 -p 50000:50000 -e JENKINS_OPTS="--argumentsRealm.passwd.admin=admin --argumentsRealm.roles.user=admin" -e JAVA_OPTS="-Djenkins.install.runSetupWizard=false" jenkins/jenkins:lts
+
+              # Check if Docker service is running
+              sudo service docker status
+              EOF
+
+  tags = {
+    Name = "JenkinsServer"
+  }
+}
+
+output "instance_ip" {
+  value = aws_instance.jenkins.public_ip
+}
